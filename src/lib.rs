868d84ea1f569f57ec4d7391270e1e05a14ec63b
Defelo <elodef42@gmail.com>
2023-05-23 15:20:11 +0200

    Use doc_auto_cfg

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,10 @@
 #![forbid(unsafe_code)]
 #![warn(clippy::dbg_macro, clippy::use_debug)]
 #![warn(missing_docs, missing_debug_implementations)]
-#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 mod auth;
 #[cfg(feature = "sea-orm")]
-#[cfg_attr(docsrs, doc(cfg(feature = "sea-orm")))]
 pub mod db;
 pub mod panic_handler;
 pub mod patch_value;
